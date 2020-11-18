6a98b50a99c3a1cf7fdcbb0275d661cf92b33b50
Senso4s <Samo-samo.simoncic@senso4s.com>
2020-11-18 09:29:40 +0100

    Add project files.

---
+++ TestOnStart/TestOnStart.Android/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
+using Android.Support.V4.Content;
+using Android;
+using Android.Support.V4.App;
+
+namespace TestOnStart.Droid
+{
+    [Activity(Label = "TestOnStart", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        int REQUEST_LOCATION = 10;
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+
+
+         
+            
+            CheckAndRequestLocationPermissionAsync();
+            //LoadApplication(new App());
+
+            
+        }
+
+        private async Task CheckAndRequestLocationPermissionAsync()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+           
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                if(status == PermissionStatus.Granted)
+                    LoadApplication(new App());
+            }
+            else
+                LoadApplication(new App());
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);           
+        }
+    }
+}
